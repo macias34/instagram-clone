ad4d18eab68818311293a49febc28bee7ace8dd7
macias34 <propiesek@gmail.com>
2023-05-03 15:25:36 +0200

    Changed file extension for PostContext from tsx to ts

--- prisma/views/instagram-clone/userdetails.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  `instagram-clone`.`User`.`id` AS `id`,
-  `instagram-clone`.`User`.`username` AS `username`,
-  `instagram-clone`.`User`.`name` AS `name`,
-  `instagram-clone`.`User`.`bio` AS `bio`,
-  `instagram-clone`.`User`.`image` AS `image`
-FROM
-  `instagram-clone`.`User`
